23c2175568e853c47cbdc5daf384463c0cd3cae1
Zumry <amzumry@gmail.com>
2022-05-16 07:41:01 +0530

    error fixed

--- search-service/src/main/java/ctseproject/searchservice/api/SearchApi.java
+++ search-service/src/main/java/ctseproject/searchservice/api/SearchApi.java
@@ -1,7 +1,6 @@
 package ctseproject.searchservice.api;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ctseproject.searchservice.domain.Products;
